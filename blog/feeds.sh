15bbeed6ccbb48de1fe83f17241fbdb89701d9cd
Laurence Willetts <wildefur@gmail.com>
2016-03-04 22:48:05 +0000

    improve making feeds

--- blog/feeds.sh
+++ blog/feeds.sh
@@ -11,7 +11,7 @@ EOF
 test -z "$1" && usage 1 || FILE="$1"
 BASEURL="http://wildefyr.net"
 
-cat << EOF
+cat > "feed.xml" << EOF
 <?xml version='1.0'?>
 <rss version='2.0' xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
@@ -27,10 +27,10 @@ posts=$(cat "$FILE" | tail -${postNum})
 
 for i in $(seq $postNum); do
     post=$(echo "$posts" | sed "$i!d")
-    echo "$post" | sed "s_* __; s_\[_<item><title>_; s_\]_</title>_; s_(_<guid>${BASEURL}_; s_)_</guid></item>_"
+    echo "$post" | sed "s_* __; s_\[_<item><title>_; s_\]_</title>_; s_(_<guid>${BASEURL}_; s_)_</guid></item>_" > feed.xml
 done
 
-cat << EOF
+cat > "feed.xml" << EOF
 </channel>
 </rss>
 EOF
