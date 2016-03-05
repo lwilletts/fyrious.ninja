fc19cab078f1b49665e8a62760adc6f6dee4bde4
Laurence Willetts <wildefur@gmail.com>
2016-03-05 17:29:24 +0000

    improve indexing scripts based on dcat's suggestions

--- ports.sh
+++ ports.sh
@@ -1,33 +1,33 @@
 #!/bin/sh
 #
-# wildefyr - 2016 (c) wtfpl
+# wildefyr - 2016 (c) MIT
 # build individual ports index.html to show hidden files
 
 HTMLROOT="/builds/wildefyr.net"
 
 html() {
-    printf '%s' "<pre><code>" > index.html
-    printf "<a href=\"../\">../</a>\n" >> index.html
+    cat 2> /dev/null > "index.html" << EOF
+<table>
+<tr class="header"><td><b>File</b></td><td><b>Last Modified</b></td><td><b>Size</b></td></tr>
+<tr><td></td></tr>
+<tr><td><a href=../>../</a></td></tr>
+EOF
 
-    find . -maxdepth 0 -exec ls -A {} \; | \
-    while IFS= read -r file; do
+    ls -A -1 | while IFS= read -r file; do
         test "$file" = "index.html" && continue
-        date=$(du --time ${file} | awk '{print $2,$3}')
-        size=$(du -hs ${file} | awk '{print $1}')
+        date=$(du --time "$file" | awk '{print $2,$3}')
+        size=$(du -hs "$file" | awk '{print $1}')
 
         test "$size" = "0" && size="-"
 
-        printf "\
-<div id=\"alignfile\">\
-<a href=\"${file}\">${file}</a>\
-</div>\
-<div id=\"aligndata\">\
-<span id=\"alignsize\">${size}</span>\t\
-<span id=\"aligndate\">${date}</span>\t\
-</div>\n" >> index.html
-    done 
+        cat 2> /dev/null >> "index.html" << EOF
+<tr><td><a href="${file}">${file}</a></td><td>${date}</td><td>${size}</td></tr>
+EOF
+    done
 
-    printf '%s\n' "</code></pre>" >> index.html
+    cat 2> /dev/null >> "index.html" << EOF
+</table>
+EOF
 }
 
 # generate html
