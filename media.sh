2df071cd1728fe1c7dd108e6b3f10fbb4f5dd2a7
Laurence Willetts <wildefur@gmail.com>
2016-03-06 13:04:09 +0000

    Update dir scripts to produce better dir output

--- media.sh
+++ media.sh
@@ -15,10 +15,15 @@ EOF
 
     ls -A -1 | while IFS= read -r file; do
         test "$file" = "index.html" && continue
+
         date=$(du --time "$file" | awk '{print $2,$3}')
         size=$(du -hs "$file" | awk '{print $1}')
 
         test "$size" = "0" && size="-"
+        directory=$(find -type d -name "${file}")
+        test ! -z "$directory" && {
+            file="$(printf '%s\n' "$directory" | cut -d'/' -f 2-)/"
+        }
 
         cat 2> /dev/null >> "index.html" << EOF
 <tr><td><a href="${file}">${file}</a></td><td>${date}</td><td>${size}</td></tr>
@@ -43,7 +48,6 @@ generate() {
 
 clean() {
     pkill wendy && printf '%s\n' "wendy killed!"
-
     find -name "index.html" -exec rm {} \;
 }
 
